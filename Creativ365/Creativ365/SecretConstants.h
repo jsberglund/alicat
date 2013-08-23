4c64d7b886b3004a3820ed057149fe9dcd3a4bfc
jbergleton <jbergleton>
2013-08-23 00:09:11 -0700

    remove tracking of secret constants

--- Creativ365/Creativ365/SecretConstants.h
+++
@@ -1,8 +0,0 @@
-// Replace with App Consumer Key
-#define kTumblrConsumerKey @"REPLACE_ME"
-
-// Replace your App Consumer Secret Key
-#define kTumblrConsumerSecret @"SECRET_KEY"
-
-// Replace your App Callback URL Key
-#define kTumblrAppCallbackURL @"APP_CALLBACK"
